66207976575462df1bb03fd46b1336e34a05d912
sneha <54199703+snehatawade@users.noreply.github.com>
2025-06-15 12:17:04 +0530

    Time: 0 ms (100%), Space: 12.6 MB (22.34%) - LeetArchive

---
+++ LeetCode/0118-pascals-triangle/0118-pascals-triangle.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        output = []
+        for i in range(numRows):
+            if(i == 0):
+                prev = [1]
+                output.append(prev)
+            else:
+                curr = [1]
+                j = 1
+                while(j < i):
+                    curr.append(prev[j-1] + prev[j])
+                    j+=1
+                curr.append(1)
+                output.append(curr)
+                prev = curr
+        return output    
