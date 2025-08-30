9f16d191a6c4a2c1da5da647601567c56979336d
sneha <54199703+snehatawade@users.noreply.github.com>
2025-08-30 20:14:24 +0530

    Delete old solution - Time: 470 ms (67.6%), Space: 0B (100%) - LeetArchive

--- LeetCode/0197-rising-temperature/0197-rising-temperature.sql
+++
@@ -1,8 +0,0 @@
-# Write your MySQL query statement below
-SELECT today.id
-FROM Weather yesterday 
-CROSS JOIN Weather today
-
-WHERE DATEDIFF(today.recordDate,yesterday.recordDate) = 1
-    AND today.temperature > yesterday.temperature
-;
